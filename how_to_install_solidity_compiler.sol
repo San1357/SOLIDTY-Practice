5cf52b4a79c47d64d0996c5fbda7277224c6f12d
San1357 <san1357@gmail.com>
2021-06-11 16:32:56 +0530

    solidity compiler installation

---
+++ how_to_install_solidity_compiler.sol
@@ -0,0 +1,35 @@
+Method 1:
+
+step 1 : First install Nodejs Package mananger.
+
+        $ sudo yum install epel-release
+
+        $ sudo yum install nodejs
+
+        $ sudo yum install npm
+
+        $ npm --version
+
+
+step 2 : Now install solidity compiler 
+
+        $ sudonpm install -g solc
+
+        $ solcjs-version 
+
+
+Method 2:
+
+step1 : pull a Docker image 
+       
+       $ docker pull ethereum/solc:stable
+
+       $ docker run ethereum/solc:stable-version
+
+       $ docker run ethereum/solc:stable -version
+       
+        
+
+
+
+
