61287bb911461ed1e7aac0749f9f31825bdc48a8
San1357 <san1357@gmail.com>
2021-06-11 19:35:37 +0530

    variables in solidity

---
+++ variables.sol
@@ -0,0 +1,36 @@
+
+VARIABLES : 
+    3 types:
+
+1- state variables
+2- Local variables
+3- Global variables 
+
+1- state variables- values permanently stored in contract storage.
+
+2- Local variables-  value present till function is executing.
+
+3- Global variables- kind of special variable which already exist in workspace & provide info 
+                     about blockchain & transaction process.
+
+                     example- msg.sender, msg.value, now etc.
+
+
+
+--> Rules of declaring Variables in Solidity :
+
+ * same Rules which have been followed in variables declaring in python.
+
+ 1 state variable :
+                |
+                |--- Public
+                |
+                |
+                |--- Private
+                |
+                |
+                |--- Internal
+                |
+                                     
+
+
